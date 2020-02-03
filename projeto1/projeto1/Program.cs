230d3b2073f231ba507c627f4b751b343db3fc01
anne nicolle zimmermann <annezdz@hotmail.com>
2020-02-02 23:42:24 -0300

    Commit 2

--- projeto1/projeto1/Program.cs
+++ projeto1/projeto1/Program.cs
@@ -10,7 +10,7 @@ namespace projeto1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Worlgsgsgd");
         }
     }
 }
